3f00f709b9adca3adb11f0e971be7e718ee2488e
Daniel <danielrudn@gmail.com>
2017-12-22 21:16:16 -0500

    configure webpack to build/watch static assets

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
+const production = process.env.NODE_ENV === 'production';
+
+const extractSassPlugin = new ExtractTextPlugin({
+  filename: '[name].bundle.css'
+});
+
+const plugins = [extractSassPlugin];
+
+if (production) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({ warnings: false }));
+}
+
+module.exports = {
+  entry: {
+    app: ['./src/public/js/index.js', './src/public/css/main.scss']
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist', 'public')
+  },
+  devtool: production ? false : 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /.scss$/,
+        use: extractSassPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins
+};
