ee223cce4727595c838eead2b49a5a2ec580a40e
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2018-11-15 18:11:09 -0500

    Configure typeorm to run migrations on startup from src/migrations

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,14 @@
 import * as path from 'path';
-import { Connection, createConnection } from 'typeorm';
+import { createConnection } from 'typeorm';
 
 export default async function() {
   const connection = await createConnection({
     type: 'postgres',
     url: process.env.POSTGRES_URL,
-    synchronize: true,
-    entities: [path.join(__dirname, 'entities', '*.js')],
+    synchronize: false,
+    entities: [path.join(__dirname, 'entities', '*')],
+    migrations: [path.join(__dirname, 'migrations', '*')],
+    migrationsRun: true,
     logging: process.env.NODE_ENV !== 'production'
   });
 
