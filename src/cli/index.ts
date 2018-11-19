b48c5674aa84b6265a8cb5f21872d651d3d0dfdf
Daniel Rudnitski <danielrudn@gmail.com>
2018-11-19 17:12:25 -0500

    Add CLI for migrations and creating OAuth2 clients (#15)
    * Add cli for running migrations in production
    
    * Add validations for oauth2client and ability to create clients from cli

---
+++ src/cli/index.ts
@@ -0,0 +1,31 @@
+import * as program from 'commander';
+import initDB from '../db';
+import OAuth2ClientService from '../services/oauth2-client-service';
+
+(async () => {
+  const connection = await initDB();
+  program.version('0.1.0', '-v, --version');
+
+  program
+    .command('migrate')
+    .action(async _ => await connection.runMigrations());
+
+  program
+    .command('createOAuth2Client <name> <redirectUris...>')
+    .option('-t, --trusted', 'Whether or not this client is trusted')
+    .action(async (name, redirectUris, cmd) => {
+      try {
+        const client = await OAuth2ClientService.create(
+          name,
+          'default description',
+          redirectUris,
+          cmd.trusted
+        );
+        console.log('Successfully created client:', client);
+      } catch (err) {
+        console.error('Error creating client:', err);
+      }
+    });
+
+  program.parse(process.argv);
+})();
