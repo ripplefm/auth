05ff7922250771907b13425f998de57736798339
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2018-11-18 19:21:19 -0500

    Database now retries connections before crashing

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,11 @@
 import * as path from 'path';
 import { createConnection } from 'typeorm';
 
-export default async function() {
-  const connection = await createConnection({
+const numRetries = process.env.DB_RETRIES || 5;
+let numTries = 0;
+
+function connect() {
+  return createConnection({
     type: 'postgres',
     url: process.env.POSTGRES_URL,
     synchronize: false,
@@ -11,6 +14,22 @@ export default async function() {
     migrationsRun: true,
     logging: process.env.NODE_ENV !== 'production'
   });
+}
 
-  return connection;
+function sleep(durationMs = 1000) {
+  return new Promise(resolve => setTimeout(resolve, durationMs));
+}
+
+export default async function() {
+  while (numTries < numRetries) {
+    try {
+      return await connect();
+    } catch (e) {
+      await sleep(1000);
+      numTries++;
+    }
+  }
+  throw new Error(
+    'Reached maximum number of retries for connecting to database'
+  );
 }
