eb92f01ea6ad3186af1e627e116be84165211448
Daniel Rudnitski <daniel.rudnitski@ibm.com>
2018-11-18 20:07:57 -0500

    add oauth2 error to error middleware

--- src/middleware/error-middleware.ts
+++ src/middleware/error-middleware.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { ValidationError } from 'class-validator';
-import InvalidInputError from '../errors/invalid-input-error';
 import { QueryFailedError } from 'typeorm';
+import InvalidInputError from '../errors/invalid-input-error';
+import OAuth2Error from '../errors/oauth2-error';
+const AuthorizationError = require('oauth2orize/lib/errors/authorizationerror');
 
 interface PrevFormFields {
   [key: string]: string;
@@ -26,5 +28,9 @@ export function errorMiddleware(
   } else if (err instanceof QueryFailedError) {
     req.flash('danger', err.message);
     return res.render(req.path.substring(1), prevFormFields);
+  } else if (err instanceof OAuth2Error || err instanceof AuthorizationError) {
+    return res.status(400).send(err.message);
+  } else if (!res.headersSent) {
+    return res.status(500).send('Unknown error occurred.');
   }
 }
